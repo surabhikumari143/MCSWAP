4339e69f9d2e93bffea91e1652707961d5e32649
surabhikumari143 <91407263+surabhikumari143@users.noreply.github.com>
2021-09-26 18:54:37 +0530

    Add files via upload

---
+++ reverse my cap.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+#include <string.h>  
+int main()  
+{  
+    char str[40];  
+    printf (" \n MY Captain  ");  
+    scanf ("%s", str);  
+      
+    
+    printf (" \n After the reverse of a string: %s ", strrev(str));  
+    return 0;  
+}
